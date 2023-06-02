f3f88681c344165abd46901d9e4a3c2792fc7a7d
Bhupendr1 <bhupendranirwan737@gmail.com>
2023-06-02 12:36:15 +0530

    today

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngIfelseThen';
+  FilterForm!: FormGroup
+  Results: any[]=[];
+  errorMessage: string = '';
+  showResultContainer=false;
+  constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.FilterForm = this.fb.group({
+      selectedOption: [0],
+      BillType: [-1],
+      RadioBtn: [],
+      Name: []
+    })
+
+  }
 }
